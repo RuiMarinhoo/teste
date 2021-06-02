79cbb1398f22f9af0e770b7f26623fed68816b59
RuiMarinhoo <ruijorgemm@hotmail.com>
2021-06-02 11:57:40 +0100

    Start

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
