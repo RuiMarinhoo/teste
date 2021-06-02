4183a7d8b0c74e8e25352085fa30b771b9213f79
RuiMarinhoo <ruijorgemm@hotmail.com>
2021-06-02 11:26:47 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+// router.get('/about',function(req,res){
+//   res.sendFile(path.join(__dirname+'/about.html'));
+// });
+
+//add the router
+app.use(express.static(__dirname));
+app.use('/', router);
+const port = process.env.port || 3000;
+const hostname = '127.0.0.1';
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
